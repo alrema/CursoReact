9bbef57f4db5718e9133399afea2a56ff8afe8d4
alrema <alrema91@gmail.com>
2024-01-01 23:05:45 +0100

    Exercise 1.11

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -11,12 +11,14 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <>
         <h1>Statistics</h1>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={total} />
-        <StatisticLine text="average" value={(good - bad) / total} />
-        <StatisticLine text="positive" value={`${(good / total) * 100}%`} />
+        <table>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={(good - bad) / total} />
+          <StatisticLine text="positive" value={`${(good / total) * 100}%`} />
+        </table>
       </>
     );
   }
