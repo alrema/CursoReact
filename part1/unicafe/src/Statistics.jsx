edd827de6580a00bb2787dfd1c4b5735fba8f29b
alrema <alrema91@gmail.com>
2024-01-01 23:00:03 +0100

    Exercise 1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,15 +1,23 @@
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
-  return (
-    <>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average: {(good - bad) / (total ? total : 1)}</p>
-      <p>positive: {good / (total ? total : 1)}%</p>
-    </>
-  );
+  if (total === 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {total}</p>
+        <p>average: {(good - bad) / total}</p>
+        <p>positive: {good / total}%</p>
+      </>
+    );
+  }
 };
 
 export default Statistics;
