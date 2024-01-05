5ce5368621d7df8a89241087bc52263bad3b702c
alrema <alrema91@gmail.com>
2024-01-05 09:49:01 +0100

    Exercise 2.2

---
+++ part2/courseinfo/src/Total.jsx
@@ -0,0 +1,15 @@
+const Total = ({ parts }) => {
+  return (
+    <p>
+      <b>
+        Total of{" "}
+        {parts.reduce((sum, part) => {
+          return sum + part.exercises;
+        }, 0)}{" "}
+        exercises
+      </b>
+    </p>
+  );
+};
+
+export default Total;
