3308f9a3d3147b68b0cb6fc1645cb43b8c91bb48
alrema <alrema91@gmail.com>
2024-01-09 14:09:59 +0100

    Exercise 2.14

--- part2/countries/src/CountryInfo.jsx
+++ part2/countries/src/CountryInfo.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+
 const CountryInfo = ({ country }) => {
+  const api_key = import.meta.env.VITE_API_KEY;
+  const basepath = import.meta.env.VITE_API_BASEPATH;
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${basepath}/current.json?key=${api_key}&q=${country.capital}`)
+      .then((response) => setWeather(response.data.current));
+  }, [api_key, basepath, country.capital]);
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -11,6 +24,19 @@ const CountryInfo = ({ country }) => {
         })}
       </ul>
       <img src={country.flags.png} />
+      {weather.condition && (
+        <>
+          <h3> Current weather in {country.capital}</h3>
+          <p>{weather.condition.text}</p>
+          <img src={weather.condition.icon} />
+          <p>
+            <b>temperature: </b> {weather.temp_c} ºC
+          </p>
+          <p>
+            <b>wind: </b> {weather.wind_kph} km/h
+          </p>
+        </>
+      )}
     </div>
   );
 };
